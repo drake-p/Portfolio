445732d25865978f712b100f387790dff888304e
Drake Parker <e.drake.p@gmail.com>
2012-11-02 16:58:42 -0400

    Tweaked various things, added missing lightbox images

--- lightbox.js
+++ lightbox.js
@@ -1,25 +1,22 @@
 jQuery(document).ready(function($) {
+	$("#lightbox").hide();
     $('.lightbox_trigger').click(function(e) {
         e.preventDefault();
         var image_href = $(this).attr("href");
 		var image_title = $(this).attr("title");
         if ($('#lightbox').length > 0) {
-            $('#content').html('<img src="' + image_href + '" /><p>' + image_title + '</p>');
-            $('#lightbox').show();
-        }
-        else {
-            var lightbox =
-            '<div id="lightbox">' +
-                '<p>Click to close</p>' +
-                '<div id="content">' + 
-                    '<img src="' + image_href +'" />' +
-					'<p>' + image_title + '</p>' +
-                '</div>' +
-            '</div>';
-            $('body').append(lightbox);
+			$('#content').hide();
+            $('#lightbox').fadeIn(750, function(){
+				$('#content').html('<img src="' + image_href + '" /><p>' + image_title + '</p>');
+				$('#content').fadeIn(500);
+			});
         }
     });
     $('#lightbox').live('click', function() { 
-        $('#lightbox').hide();
+        $('#lightbox').fadeOut(500);
+		$('#content').empty();
     });
 });
+
+//This wants a next and back function to be called by arrow keys and buttons
+//This also wants ESC key functionality!
