c3f760d8f30b1cf1670ee49359a3e6529a943d95
Drake Parker <e.drake.p@gmail.com>
2012-11-15 16:28:37 -0500

    Working on lightbox

---
+++ lightbox-new.js
@@ -0,0 +1,25 @@
+$(document).ready(
+	function($) {
+    $("#lightbox").hide();
+    
+    // Show lightbox on thumbnail click
+    $('.lightbox_trigger').click(function(e){
+        e.preventDefault();
+        var image_href = $(this).attr("href");
+        var image_title = $(this).attr("title");
+		$('#content').hide();
+        $('#lightbox').fadeIn(750, function(){
+			$('#content').empty().html('<img src="' + image_href + '" /><p>' + image_title + '</p>').fadeIn(500);
+		});
+    });
+        
+    // Close lightbox
+    $('#lightbox').live('click', function() { 
+        $('#lightbox').fadeOut(500);
+    });
+	$(document).keyUp(function(event) {
+		if(event.keyCode == 27){
+        	$('#lightbox').fadeOut(500);
+		};
+	});
+});
---
+++ lightbox-old.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($) {
+	$("#lightbox").hide();
+	
+	// Show lightbox, load content on thumbnail click
+    $('.lightbox_trigger').click(function(e) {
+        e.preventDefault();
+        var image_href = $(this).attr("href");
+		var image_title = $(this).attr("title");
+        $('#content').hide();
+        $('#lightbox').fadeIn(750, function(){
+            $('#content').html('<img src="' + image_href + '" /><p>' + image_title + '</p>');
+            $('#content').fadeIn(500);
+        });
+    });
+	
+	// Advance lightbox
+	$('#lbleft').click(function() {
+		
+	})
+	
+	// Close lightbox
+    $('#lightbox').live('click', function() { 
+        $('#lightbox').fadeOut(500);
+		$('#content').empty();
+    });
+});
+
+//This wants a next and back function to be called by arrow keys and buttons
+//This also wants ESC key functionality!
